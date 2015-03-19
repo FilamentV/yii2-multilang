60ec8fcb01a9a64502f2b4ec24c80bad75013312
FilamentV <vortex.filament@gmail.com>
2015-03-20 00:21:24 +0200

    init

---
+++ Request.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace thread\components;
+
+use thread\components\MultiLanguage;
+
+/**
+ * Class Request
+ * Common Імплементація мов в Request [[Request]]
+ * @package thread\components
+ * @author FilamentV <vortex.filament@gmail.com>
+ * @copyright (c) 2014, Thread
+ * @version 19/03/2015
+ */
+final class Request extends \yii\web\Request {
+
+    private $_requestUri;
+
+    /**
+     * 
+     * @return string
+     */
+    protected function resolveRequestUri() {
+        if ($this->_requestUri === null)
+            $this->_requestUri = MultiLanguage::processLangInUrl(parent::resolveRequestUri());
+
+        return $this->_requestUri;
+    }
+
+    /**
+     * 
+     * @return string
+     */
+    public function getOriginalUrl() {
+        return $this->getOriginalRequestUri();
+    }
+
+    /**
+     * 
+     * @return string
+     */
+    public function getOriginalRequestUri() {
+        return MultiLanguage::addLangToUrl($this->resolveRequestUri());
+    }
+
+}
---
+++ UrlManager.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace thread\components;
+
+use thread\components\MultiLanguage;
+
+/**
+ * Class UrlManager
+ * Common Імплементація мов в UrlManager [[UrlManager]]
+ * @package thread\components
+ * @author FilamentV <vortex.filament@gmail.com>
+ * @copyright (c) 2014, Thread
+ * @version 19/03/2015
+ */
+final class UrlManager extends \yii\web\UrlManager {
+
+    public function createUrl($params) {
+        return MultiLanguage::addLangToUrl(parent::createUrl($params));
+    }
+
+    public function getBaseUrl() {
+        $this->setBaseUrl(MultiLanguage::getBaseUrl());
+        return parent::getBaseUrl();
+    }
+
+}
